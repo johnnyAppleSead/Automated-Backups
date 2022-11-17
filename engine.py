b6e17399cf193147a7978713bdb32f15816475fe
jalex135 <will.alexander135@gmail.com>
2022-11-16 18:44:53 -0600

    Pushing working files

--- engine.py
+++ engine.py
@@ -1,7 +1,8 @@
-from save_files import File
+from file import File
 import json
 import engine_util as util
 import shutil
+
 
 class Engine:
     def __init__(self):
@@ -10,19 +11,20 @@ class Engine:
         
     def import_config(self, file_path):
         try:
+            print("Attempting to open filepath: " + file_path)
             file = open(file_path)
             self.config = json.loads(file.read())
         except:
             print("An error occurred while attempting to import the config file")
 
     def parse(self):
-        file_configs = self.config["files"]
-        if(util.empty(file_configs) == False):
+        file_configs = self.config['files']
+        if not util.empty(file_configs):
             for config in file_configs:
                 source = config["source"]
                 target = config["target"]
 
-                if(util.empty(source) == False and util.empty(target) == False):
+                if util.empty(source) == False and util.empty(target) == False:
                     copied_file_name = util.get_file_name(source)
                     file = File(source, target, copied_file_name)
                     self.files.append(file)
@@ -36,10 +38,11 @@ class Engine:
             print("Copy process ended for " + file.source + " to " + file.target)
 
     def start(self):
-        self.import_config("D:\\Projects\\Python\\Automated Backups\\config.json")
-        self.import_config("D:\Projects\Python\Automated Backups\config.json")
+        self.import_config("D:\\Github Projects\\Automated Backups\\config.json")
+        #self.import_config("D:\Projects\Python\Automated Backups\config.json")
         self.parse()
         self.copy()
+
 
 engine = Engine()
 engine.start()
